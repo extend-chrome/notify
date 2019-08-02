ab0c27c002c0c6885bcfcc317b3848f560a0d3d1
Jack Steam <teamsteamdev@gmail.com>
2019-08-02 09:42:12 -0500

    Remove bad type declaration

--- index.d.ts
+++ index.d.ts
@@ -4,18 +4,19 @@ import chromep from 'chrome-promise'
 // Definitions by: Jack and Amy Steam <https://jackandamy.rocks>
 /// <reference types="chrome" />
 
-export interface NotifyOptions
-  extends chrome.notifications.NotificationOptions {
-  message: string
-  /**
-   * This only works if the background page is persistent.
-   */
-  onClick?: (id: string) => void
-  /**
-   * This only works if the background page is persistent.
-   */
-  buttons?: [(id: string) => void, (id: string) => void]
-}
+// TODO: fix NotifyOptions
+// export interface NotifyOptions
+//   extends chrome.notifications.NotificationOptions {
+//   message: string
+//   /**
+//    * This only works if the background page is persistent.
+//    */
+//   onClick?: (id: string) => void
+//   /**
+//    * This only works if the background page is persistent.
+//    */
+//   buttons?: [(id: string) => void, (id: string) => void]
+// }
 
 export function notify(message: string): Promise<string>
 export namespace notify {
