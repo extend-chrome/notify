d278ba20f1056c82415491dad3e299236272444c
Jack Steam <teamsteamdev@gmail.com>
2019-08-02 09:55:01 -0500

    Fix build folder and types in package

--- rollup.config.js
+++ rollup.config.js
@@ -5,12 +5,12 @@ export default [
     input: 'src/index.js',
     output: [
       {
-        file: 'build/bundle-esm.js',
+        file: 'lib/index-esm.js',
         format: 'esm',
         sourcemap: 'inline',
       },
       {
-        file: 'build/bundle-cjs.js',
+        file: 'lib/index-cjs.js',
         format: 'cjs',
         sourcemap: 'inline',
       },
